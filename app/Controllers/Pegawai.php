6e9ff2236a192750c4137c77a5d015f47f478a4d
iskndrm4 <iskndrm4@gmail.com>
2022-12-26 22:18:22 +0700

    get, by id, update, delete

--- app/Controllers/Pegawai.php
+++ app/Controllers/Pegawai.php
@@ -15,23 +15,85 @@ class Pegawai extends BaseController
 
     public function index()
     {
-  
+        $data = $this->model->orderBy('nama', 'asc')->findAll();
+        return $this->respond($data, 200);
+    }
+
+    public function show($id = null)
+    {
+        $data = $this->model->where('id', $id)->findAll();
+        if ($data) {
+            return $this->respond($data, 200);
+        } else {
+            return $this->failNotFound("Data tidak ditemukan untuk id $id");
+        }
     }
 
     public function create()
     {
-        $data = [
-            'nama' => $this->request->getVar('nama'),
-            'email' => $this->request->getVar('email'),
-        ];
-        $this->model->save($data);
+        // $data = [
+        //     'nama' => $this->request->getVar('nama'),
+        //     'email' => $this->request->getVar('email'),
+        // ];
+        // $this->model->save($data);
+
+        $data = $this->request->getPost();
+
+        if (!$this->model->save($data)) {
+            return $this->fail($this->model->errors());
+        }
+
         $response = [
             'status' => 200,
+            'error' => false,
             'messages' => [
                 'success' => 'Berhasil memasukan data pegawai'
             ],
         ];
 
         return $this->respond($response);
+    }
+
+    public function update($id = null)
+    {
+        $data = $this->request->getRawInput();
+        $data['id'] = $id;
+
+        $isExists = $this->model->where('id', $id)->findAll();
+        if (!$isExists) {
+            return $this->failNotFound("Data tidak ditemukan untuk id $id");
+        }
+
+        if (!$this->model->save($data)) {
+            return $this->fail($this->model->errors());
+        }
+
+        $response = [
+            'status' => 200,
+            'error' => false,
+            'messages' => [
+                'success' => "Data dengan id $id berhasil di update" 
+            ]
+        ];
+
+        return $this->respond($response);
+    }
+
+    public function delete($id = null)
+    {
+        $data = $this->model->where('id', $id)->findAll();
+        if ($data) {
+            $this->model->delete($id);
+            $response = [
+                'status' => 200,
+                'error' => false,
+                'messages' => [
+                    'success' => "Data berhasil dihapus"
+                ]
+            ];
+            return $this->respondDeleted($response);
+        } else {
+            return $this->failNotFound("Data tidak ditemukan");
+        }
     }
 }
--- app/Models/ModelPegawai.php
+++ app/Models/ModelPegawai.php
@@ -9,4 +9,18 @@ class ModelPegawai extends Model
     protected $table = "pegawai";
     protected $primaryKey = "id";
     protected $allowedFields = ['nama', 'email',];
+
+    protected $validationRules = [
+        'nama' => 'required',
+        'email' => 'required|valid_email'
+    ];
+    protected $validationMessages = [
+        'nama' => [
+            'required' => 'Silahkan masukkan nama'
+        ],
+        'email' => [
+            'required' => 'Silahkan masukkan email',
+            'valid_email' => 'Email yang di masukkan tidak valid'
+        ]
+    ];
 }
